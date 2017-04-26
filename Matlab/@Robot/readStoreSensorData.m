b1bc5244304a8622da1e613e61b9281a029456ae
Alex Wadell <awadell@gmail.com>
2017-04-26 14:20:39 -0400

    Updated readStoreSensorData

--- Matlab/@Robot/readStoreSensorData.m
+++ Matlab/@Robot/readStoreSensorData.m
@@ -37,111 +37,39 @@ end
 
 % Check if the Robot is in debugMode
 if ~robot.debugMode
-	% If optional input 'BeaconMode' was not provided.  Assign default value
-	if nargin<2, BeaconMode = 1; end
-	
 	%% read truth pose (from overhead localization system)
-	if robot.enableOverhead
-		try
-			[px, py, pt] = OverheadLocalizationCreate(tagNum);
-			if (px == 0 && py == 0 && pt == 0 && ~isa(tagNum,'CreateRobot'))
-				disp('Overhead localization lost the robot!')
-				noRobotCount = noRobotCount + 1;
-			else
-				poseX = px; poseY = py; poseTheta = pt;
-				dataStore.truthPose = [dataStore.truthPose ; ...
-					toc poseX poseY poseTheta];
-				noRobotCount = 0;
-			end
-		catch
-			disp('Error retrieving or saving overhead localization data.');
+	try
+		[px, py, pt] = OverheadLocalizationCreate(tagNum);
+		if (px == 0 && py == 0 && pt == 0 && ~isa(tagNum,'CreateRobot'))
+			disp('Overhead localization lost the robot!')
+			noRobotCount = noRobotCount + 1;
+		else
+			poseX = px; poseY = py; poseTheta = pt;
+			dataStore.truthPose = [dataStore.truthPose ; ...
+				toc poseX poseY poseTheta];
+			noRobotCount = 0;
 		end
-	else
-		% Overhead Localization is disabled
-		dataStore.truthPose = [dataStore.truthPose ; toc 0 0 0];
+	catch
+		disp('Error retrieving or saving overhead localization data.');
 	end
 
-	
-	% read odometry distance & angle
-	try
-		deltaD = DistanceSensorRoomba(CreatePort);
-		deltaA = AngleSensorRoomba(CreatePort);
-		dataStore.odometry = [dataStore.odometry ; ...
-			toc deltaD deltaA];
-	catch
-		disp('Error retrieving or saving odometry data.');
-	end
-	
-	%% read lidar data
-	% try
-	%     if isa(CreatePort,'CreateRobot')
-	%         % Read lidar data from Simulator
-	%         lidarScan = LidarSensorCreate(CreatePort);
-	%         dataStore.lidar = [dataStore.lidar ; toc lidarScan];
-	%     else
-	%         % Lidar data is not available from the iRobot Create
-	%     end
-	% catch
-	%     disp('Error retrieving or saving lidar data.');
-	% end
-	
-	%% read bump data
-	% Ignore drop and caster sensors
-	
-	try
-		[BumpRight, BumpLeft, ~, ~, ~, BumpFront] = ...
-			BumpsWheelDropsSensorsRoomba(CreatePort);
-		dataStore.bump = [dataStore.bump ; toc ...
-			BumpRight BumpLeft BumpFront];
-	catch
-		disp('Error retrieving or saving bump sensor data.');
-	end
-	
-	%% read sonar data
-	try
-		if isa(SonarPort,'CreateRobot')
-			% Read sonar data from Simulator
-			sonarR = ReadSonar(SonarPort,1);
-			sonarF = ReadSonar(SonarPort,2);
-			sonarL = ReadSonar(SonarPort,3);
+	cmd = {'GOM', 'GRM', 'GIM'};
+	name = {'odometry', 'range', 'IMU'};
+	for i = 1:length(cmd)
+		% read sensor data from robot
+		try
+			[resp, time] = robot.sendPacket(cmd{i});
 			
-			% Check for empty returns (out of range readings)
-			if isempty(sonarR), sonarR = NaN; end
-			if isempty(sonarF), sonarF = NaN; end
-			if isempty(sonarL), sonarL = NaN; end
-		else
-			% Read sonar data from the iRobot Create
-			sonar = ReadSonar(SonarPort,[1,2,3]);
-			sonarR = sonar(1); sonarF = sonar(2); sonarL = sonar(3);
+			% Parse resp
+			data = sscanf(resp, '%f');
+			
+			% Append to dataStore
+			dataStore.(name{i}) = [dataStore.(name{i}) ; ...
+				time data'];
+		catch
+			disp('Error retrieving or saving odometry data.');
 		end
-		dataStore.sonar = [dataStore.sonar ; toc sonarR sonarF sonarL];
-	catch
-		disp('Error retrieving or saving sonar data.');
 	end
-	
-	%% read camera data (beacons)
-	try
-		if isa(BeaconPort,'CreateRobot')
-			switch BeaconMode
-				case 0
-					% Use blob detection
-					[angle, dist, color]= CameraSensorCreate(BeaconPort);
-					beacons = [color, angle, dist];
-				case 1
-					% Read AR Tags
-					[Xvec,Yvec,Zvec,ROTvec,TAGvec] = ReadBeacon(BeaconPort);
-					beacons = [TAGvec,Xvec,Yvec,Zvec,ROTvec];
-			end
-		else
-			% Read beacon data from the iRobot Create
-			beacons = ReadBeacon(BeaconPort);
-		end
-		if ~isempty(beacons)
-			dataStore.beacon = [dataStore.beacon ; repmat(toc,size(beacons,1),1) beacons];
-		end
-	catch
-		%disp('Error retrieving or saving camera data.');
-	end	
 else
 	%% Debug Mode on -> Read from supplied datastore
 	
